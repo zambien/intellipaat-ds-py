e061220b01caa81059cbac691430bda7914a307d
zambien <zambien1977@yahoo.com>
2020-06-19 14:21:25 -0400

    up to minute 37, cleaning data

--- README.md
+++ README.md
@@ -16,5 +16,5 @@ Verify everything is installed
 `python -c "import pandas as pd"`
 
 
-I used pandas_notes.py instead of a Jupyter notebook to track my progres.
+I used pandas_notes.py instead of a Jupyter notebook to track my progress.
 
--- pandas_notes.py
+++ pandas_notes.py
@@ -80,3 +80,96 @@ print (df1.merge(df2, on='Player', how='left'))
 print_header("Right Merge")
 
 print (df1.merge(df2, on='Player', how='right'))
+
+print_header("Outer Merge")
+
+print (df1.merge(df2, on='Player', how='outer'))
+
+## Note, difference between join and merge:
+##   - Merge works on the attributes in a Data Frame
+##   - Join works on an index in a data frame
+
+print_header("Join Operation")
+
+player = ['Player1','Player2','Player3']
+point = [8,9,6]
+title = ['Game1','Game2','Game3']
+
+df3 = pd.DataFrame({'Player':player, 'Points':point, 'Title':title}, index = ['L1', 'L2', 'L3'])
+
+print (df3)
+
+player = ['Player1','Player5','Player6']
+power = ['punch','kick','elbow']
+title = ['Game1','Game5','Game6']
+
+df4 = pd.DataFrame({'Players':player, 'Power':power, 'Titles':title}, index = ['L2', 'L3', 'L4'])
+
+print (df4)
+
+print_header("Inner Join")
+
+print(df3.join(df4, how='inner'))
+
+print_header("Left Join")
+
+print(df3.join(df4, how='left'))
+
+print_header("Right Join")
+
+print(df3.join(df4, how='right'))
+
+print_header("Outer Join")
+
+print(df3.join(df4, how='outer'))
+
+print_header("Concatenate two data frames")
+
+print(pd.concat([df3,df4]))
+
+print_header("Importing and Analyzing Datasets")
+
+cars = pd.read_csv("mtcars2.csv")
+
+print (cars)
+
+print_header("pd.read_csv(\"mtcars2.csv\") is a data frame:")
+print(type(cars))
+
+print_header("first 5 records")
+print(cars.head())
+
+print_header("first 10 records")
+print(cars.head(10))
+
+print_header("last 5 records")
+print(cars.tail())
+
+print_header("last 10 records")
+print(cars.tail(10))
+
+print_header("shape - what is the shape of the data?")
+print("shape is rows: " + str(cars.shape[0]) + ", columns: " + str(cars.shape[0]))
+
+print(cars.info(null_counts=True))
+
+print_header("cars.mean")
+print(cars.mean())
+
+print_header("cars.median")
+print(cars.median())
+
+print_header("cars.std - standard deviation")
+print(cars.std())
+ 
+print_header("cars.max - max values")
+print(cars.max())
+
+print_header("cars.min - min values")
+print(cars.min())
+
+print_header("cars.count - non-null values")
+print(cars.count())
+
+print_header("cars.describe - summary of all descriptor statistics")
+print(cars.describe())
